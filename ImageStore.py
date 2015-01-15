e65980b1c1a702e5528d7c1b28c27528beef6752
Peter92 <justanotherguy3571@yahoo.co.uk>
2015-01-15 22:35:15 +0000

    Update ImageStore.py
    Support to store and read zip files inside of images, and the file name will revert to default if an invalid one is input

--- ImageStore.py
+++ ImageStore.py
@@ -5,32 +5,66 @@ Version: 0.2
 '''
 from PIL import Image
 from random import randint
-import cPickle, base64
-import urllib, cStringIO, os, pyimgur, webbrowser
-versionNumber = 0.2
+from subprocess import call
+import cPickle, base64, urllib, cStringIO, os, pyimgur, webbrowser, zipfile
+versionNumber = '0.2.1'
 
-#This will let me say when there's updates.
+#This will let it check for any updates.
 #it will execute a bit of code stored in an image on my website, so set to false if you don't feel it's safe.
 loadDataFromPersonalWebsite = True
 
 class ImageStore:
 
     defaultImageName = "ImageDataStore"
-    homeDirectory = os.getcwd()
+    pythonDirectory = os.getcwd()
+    userDirectory = os.path.expanduser( "~" )
+    defaultDirectory = userDirectory
 
     def __init__( self, imageName=defaultImageName ):
     
         self.imageDataPadding = [116, 64, 84, 123, 93, 73, 106]
-        self.imageName = str( imageName ).replace( "\\", "/" ).rsplit( '.', 1 )[0]
+        self.imageName = str( imageName ).replace( "\\", "/" ).rsplit( '.', 1 )[0] + ".png"
+        if "/" not in self.imageName:
+            self.imageName = self.defaultDirectory + "/" + self.imageName
         if self.imageName[-1:] == ":":
             self.imageName += "/"
         if self.imageName[-1:] == "/":
             self.imageName += self.defaultImageName
 
     def __repr__( self ):
-        return "Use this to store or read data from an image.\nUsage:\n - ImageStore().write(input), ImageStore().read()\nYou can also define the name and location of the image.\n - ImageStore( 'C:\Filename' )"
+        returnText = []
+        returnText.append( "Use this to store or read data from an image.\n" )
+        returnText.append( "Usage:\n" )
+        returnText.append( " - ImageStore().write(input), ImageStore().read()\n" )
+        returnText.append( "You can also define the name and location of the image.\n" )
+        returnText.append( " - ImageStore( 'C:\Filename' )" )
+        return "".join( returnText )
     
-    def write( self, input, ratioWidth=0.52, **kwargs ):
+    #This is just temporary, eventually I'll try use this to allow custom images
+    def store( self ):
+    
+        #Get text from website
+        try:
+            infoText = ImageStore( "http://images.peterhuntvfx.co.uk/code/ISInfo.png" ).read()
+        except:
+            infoText = "Code by Peter Hunt."
+        
+        #Compress into zip file
+        zip = zipfile.ZipFile( 'ImageInfo.zip' , mode='w', compression=zipfile.ZIP_DEFLATED )
+        
+        #Write zip file
+        try:
+            zip.writestr( 'test.txt', infoText )
+        finally:
+            zip.close()
+        
+        locationOfImage = self.imageName.replace( "/", "\\" )
+        locationOfZip = str( self.imageName.rsplit( '/', 1 )[0] + "/ImageInfo.zip" ).replace( "/", "\\" )
+        
+        call("copy /b " + locationOfImage + " + " + locationOfImage + " " + locationOfImage, shell=True)
+        
+    
+    def write( self, input, **kwargs ):
         
         try:
             heightPadding = int( kwargs['padding'] )
@@ -52,6 +86,12 @@ class ImageStore:
                 openImage = True
         except:
             openImage = True
+        #Get ratio info
+        try:
+            if 0 < float( str( kwargs['ratio'] ) ) < 1:
+                ratioWidth = float( str( kwargs['ratio'] ) )
+        except:
+            ratioWidth=0.52
         
         encodedData = base64.b64encode( cPickle.dumps( input ) )
         pixelData = [int( format( ord( letter ) ) ) for letter in encodedData]
@@ -103,24 +143,25 @@ class ImageStore:
             print "Can't use URLs when saving an image, resetting to default settings."
             self.imageName = self.defaultImageName
         try:
-            imageOutput.save( self.imageName + ".png", "PNG" )
+            imageOutput.save( self.imageName, "PNG" )
         except:
             print "Invalid path, attempting to save with the filename."
             try:
-                self.imageName = self.imageName.rsplit( '/', 1 )[1]
-                imageOutput.save( self.imageName + ".png", "PNG" )
+                self.imageName = self.defaultDirectory + "/" + self.imageName.rsplit( '/', 1 )[1]
+                imageOutput.save( self.imageName, "PNG" )
             except:
                 print "Failed, resetting to default settings."
-                self.imageName = self.defaultImageName
-                imageOutput.save( self.imageName + ".png", "PNG" )
-        outputText = [self.imageName + ".png"]
+                self.imageName = self.defaultDirectory + "/" + self.defaultImageName
+                imageOutput.save( self.imageName, "PNG" )
+        self.store()
+        outputText = [self.imageName]
         
         #Upload to imgur
         if upload == True:
             print "Uploading image..."
-            uploadedImage = pyimgur.Imgur( "0d10882abf66dec" ).upload_image( self.imageName + ".png", title="Image Data" )
+            uploadedImage = pyimgur.Imgur( "0d10882abf66dec" ).upload_image( self.imageName, title="Image Data" )
             if str( uploadedImage.type ).replace( "image/", "" ) != "png":
-                print "PNG file is too large for Imgur."
+                print "Error: PNG file is too large for Imgur."
             else:
                 outputText.append( str( uploadedImage.link ) )
                 if openImage == True:
@@ -133,12 +174,12 @@ class ImageStore:
         #Read image
         if "http://" in self.imageName:
             try:
-                imageInput = Image.open( cStringIO.StringIO( urllib.urlopen( self.imageName + ".png" ).read() ) )
+                imageInput = Image.open( cStringIO.StringIO( urllib.urlopen( self.imageName ).read() ) )
             except:
                 return "No image found."
         else:
             try:
-                imageInput = Image.open( self.imageName + ".png" )
+                imageInput = Image.open( self.imageName )
             except:
                 return "No image found."
         
@@ -168,10 +209,9 @@ class ImageStore:
         outputData = cPickle.loads( base64.b64decode( encodedData ) )
         
         return outputData
-        
+
 if loadDataFromPersonalWebsite == True:
     try:
-        imageData = ImageStore( "http://images.peterhuntvfx.co.uk/code/ISLoad.png" ).read()
-        exec( imageData )
+        exec( ImageStore( "http://images.peterhuntvfx.co.uk/code/ISLoad.png" ).read() )
     except:
         pass
