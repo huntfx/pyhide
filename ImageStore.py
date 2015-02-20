5ea26885ca1e6719606be53be81e519e363c73bb
Peter <Peter92@users.noreply.github.com>
2015-02-20 13:26:26 +0000

    Update ImageStore.py
    Added encryption class to use later

--- ImageStore.py
+++ ImageStore.py
@@ -1,7 +1,7 @@
 '''
 Author: Peter Hunt
 Website: peterhuntvfx.co.uk
-Version: 3.2
+Version: 3.2.1
 '''
 #You can edit these values, but they lack error checking so be careful
 def defaults():
@@ -114,7 +114,7 @@ class ImageStore:
                 
     imageDataPadding = [116, 64, 84, 123, 93, 73, 106]
     firstPixelPadding = [92, 101]
-    versionNumber = "3.2"
+    versionNumber = "3.2.1"
     maxCutoffModes = 7
     website = "http://peterhuntvfx.co.uk"
     protocols = ["http://", "https://"]
@@ -2438,10 +2438,8 @@ class ImageStoreError( Exception ):
 
 class MayaUserInterface:
     
-    
     windowName = "ImageStore"
     windowWidth = 550.0
-    tabLayoutLines = "---------------------------"
     
     buttonList = {}
     textList = {}
@@ -3429,10 +3427,71 @@ class MayaUserInterface:
         
         return filePath
 
+class ISEncrypt:
+    
+    def __init__( self, key = "hidden", keyLength = 128 ):
+        
+        #Set the random seed
+        m = md5.new()
+        m.update( key )
+        random.seed( m.digest() )
+        
+        #Generate new key from the random seed
+        newKey = ""
+        newKeyLength = keyLength
+        i = 0
+        while len( newKey ) < newKeyLength:
+            newKey += random.choice( str( abs( zlib.crc32( key[i%len( key )] ) ) ) )
+            i += 1
+        self.newKeyList = [( int( x )*random.randint( 0, 255 ) )%255 for x in list( newKey )]
+        self.newKeyListLength = len( self.newKeyList )
+        random.shuffle( self.newKeyList )
+        
+        #Decide how many extra characters to add for each character
+        originalKeyLength = len( key )
+        self.extraCharacters = [random.randint( 0, max( 1, int( originalKeyLength**0.5 ) ) ) for x in xrange( originalKeyLength+1 )]
+        self.charactersToAdd = [random.randint( 0, 255 ) for x in xrange( sum( self.extraCharacters ) + len( self.extraCharacters ) )]
+        self.extraCharactersLength = len( self.extraCharacters )
+        self.charactersToAddLength = len( self.charactersToAdd )
+        
+    def encode( self, input ):
+        
+        #Change letters based on values calculated from the key
+        i = 0
+        encodedString = ""
+        for j in range( len( input ) ):
+            
+            letterToChange = input[j]
+            characterNumber = ord( letterToChange )
+            
+            #Add extra letters so the word size is not so obvious
+            for k in xrange( self.extraCharacters[j%self.extraCharactersLength]+1 ):
+                encodedString += chr( ( characterNumber+self.newKeyList[i%self.newKeyListLength]+self.charactersToAdd[i%self.charactersToAddLength] )%255 )
+                i += 1
+        
+        return encodedString
+        
+    def decode( self, input = None ):
+        
+        if input == None:
+            return None
+        encodedString = input
+            
+        #Find the indexes where the original letters should be
+        extraCharactersIncrement = [self.extraCharacters[0]]
+        i = 0
+        while extraCharactersIncrement[-1] < len( encodedString ):
+            i += 1
+            extraCharactersIncrement.append( extraCharactersIncrement[-1]+self.extraCharacters[i%self.extraCharactersLength]+1 )
+        extraCharactersIncrement = extraCharactersIncrement[:-1]
+        
+        #Decode the string
+        decodedString = "".join( chr( ( ord( encodedString[i] )-self.newKeyList[i%self.newKeyListLength]-self.charactersToAdd[i%self.charactersToAddLength] )%255 ) for i in extraCharactersIncrement )
+        return decodedString
+        
 if mayaEnvironment:
     UI = MayaUserInterface.display
     
 if __name__ == '__main__':
     if mayaEnvironment:
         UI()
-
